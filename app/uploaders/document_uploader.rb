ecd623d0e9ede6b23d14bf35368f59ba24d3060b
Alexey Krivitsky <alexeykrivitsky@gmail.com>
2017-12-21 17:54:17 +0100

    using aws in prod and files in dev

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -2,9 +2,11 @@
 # Carrierwave uploader for docuemnts.
 class DocumentUploader < CarrierWave::Uploader::Base
 
-  #storage :file
-  storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
