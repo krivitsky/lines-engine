d0b93ce77c71a56a58d2bd22dc0c2d9c8c05ae6b
Alexey Krivitsky <alexeykrivitsky@gmail.com>
2018-06-15 15:03:04 +0200

    merging users

--- app/controllers/lines/sessions_controller.rb
+++ app/controllers/lines/sessions_controller.rb
@@ -10,17 +10,32 @@ module Lines
     
     # Renders form for creating a new article 
     def new
-    end
+      #redirect_to "/login"
 
-    # Authenticate user and create a new session.
-    def create
-      user = Lines::User.find_by(email: params[:email])
-      if user && user.authenticate(params[:password])
+      user = current_user
+
+      if user && user.is_admin? #&& user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect_to admin_root_url
       else
         flash.now[:error] = t('lines.login_error')
-        render "new"
+        redirect_to "/login"
+      end
+    end
+
+    # Authenticate user and create a new session.
+    def create
+      #user = Lines::User.find_by(email: params[:email])
+      #user = ::User.find_by(email: params[:email])
+
+      user = current_user
+
+      if user && user.is_admin? #&& user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_root_url
+      else
+        flash.now[:error] = t('lines.login_error')
+        redirect_to "/login"
       end
     end
 
